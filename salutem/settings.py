d28abd57378f6e9bce8a9f8d9ba384c1c98a14e5
Andrey Nechypurenko <andreynech@gmail.com>
2014-05-20 23:55:53 +0200

    Fix database settings error.

--- salutem/settings.py
+++ salutem/settings.py
@@ -51,6 +51,18 @@ ROOT_URLCONF = 'salutem.urls'
 
 WSGI_APPLICATION = 'salutem.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+DATABASES = {}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
